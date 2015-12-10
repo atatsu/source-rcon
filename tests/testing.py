9290656f009dfa71db09a870c873cea35a66acf3
atatsu <nathan.lundquist@gmail.com>
2015-12-10 01:19:29 -0600

    added `FuncToolsPartialMatcher`

--- tests/testing.py
+++ tests/testing.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 LOG = logging.getLogger(__name__)
 
@@ -37,3 +38,19 @@ class ListenerConnectionMixin(ListenerMixin, ConnectionMixin):
 
     async def make_listener_and_connect(self):
         await self.conn.connect()
+
+
+class FuncToolsPartialMatcher:
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def __eq__(self, other):
+        if not isinstance(other, functools.partial):
+            return False
+
+        assert other.func == self.func, 'Expected func: {!r}, Actual func: {!r}'.format(self.func, other.func)
+        assert other.args == self.args, 'Expected args: {!r}, Actual args: {!r}'.format(self.args, other.args)
+        assert other.keywords == self.kwargs, 'Expected kwargs: {!r}, Actual kwargs {!r}'.format(self.kwargs, other.kwargs)
+        return True
