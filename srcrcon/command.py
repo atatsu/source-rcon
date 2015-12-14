46cb340aa13041524324558594eb5a633acd67e6
atatsu <nathan.lundquist@gmail.com>
2015-12-13 21:14:06 -0600

    Documented `command.Command`.

--- srcrcon/command.py
+++ srcrcon/command.py
@@ -1,45 +1,56 @@
+from argparse import Namespace
+from abc import ABCMeta, abstractmethod
 import logging
 LOG = logging.getLogger(__name__)
 
 from .utils import fancy
-from .exceptions import InvalidCommandError
 
 
-class Command:
+class Command(metaclass=ABCMeta):
 
+    #: Name of the command. This is the value that gets added to
+    #: the parser and used as an actual subcommand.
     name = None
+    #: Help text for the parser subcommand.
     help = None
+    #: Arguments for the subcommand. These get added to the subcommand
+    #: parser arguments. list(dict(name='arg name', help='arg help'))
     args = []
-    command_text = None
+    #: Actual text for the command. Args parsed from subcommand will be passed
+    #: in as a `string.format` so use tokens that match `Command.args`.
+    command_fmt = None
 
-    success = fancy('{command!r} succeeded.', fg='green')
-    failure = fancy('{command!r} failed!', fg='red')
-    response = fancy('{response}', fg='yellow')
-    command = None
+    #: Message to display upon command success. Formatting will be applied
+    #: with the command text so include a `{command_text}` token
+    #: if you want it as part of the resulting string.
+    success_fmt = fancy('{command_text!r} succeeded.', fg='green')
+    #: Message to display upon command failure. Formatting will be applied
+    #: with the command text so include a `{command_text}` token
+    #: if you want it as part of the resulting string.
+    failure_fmt = fancy('{command_text!r} failed!', fg='red')
+    #: Response from the server. Formatting will be applied with the server's
+    #: response so include a `{response}` token if you want it as part of the
+    #: resulting string.
+    response_fmt = fancy('{response}', fg='yellow')
 
-    def __init__(
-        self,
-        command: str = None,
-        success: str = None,
-        failure: str = None,
-        response: str = None
-    ) -> None:
-        self.success = success or self.success
-        self.failure = failure or self.failure
-        self.response = response or self.response
-        self.command = command or self.command
+    def __init__(self, parsed_args: Namespace) -> None:
+        self._args = parsed_args
 
-        if not self.command:
-            raise InvalidCommandError('Expected a command string')
+    def success(self) -> str:
+        return self.success_fmt.format(command_text=str(self))
 
-        self.success = self.success.format(command=self.command)
-        self.failure = self.failure.format(command=self.command)
+    def failure(self) -> str:
+        return self.failure_fmt.format(command_text=str(self))
 
-    def get_command_text(self, args) -> str:
-        raise NotImplementedError
+    def response(self, response: str):
+        return self.response_fmt.format(response=response)
+
+    @abstractmethod
+    def validate(self, response: str) -> bool:
+        return True
 
     def __str__(self) -> str:
-        return str(self.command)
+        return self.command_fmt.format(**vars(self._args))
 
     def __repr__(self) -> str:
         return repr(self.command)
--- tests/command-tests.py
+++ tests/command-tests.py
@@ -1,28 +1,46 @@
 from unittest import TestCase
+from argparse import Namespace
 
 from srcrcon.command import Command
 from srcrcon.utils import fancy
-from srcrcon.exceptions import InvalidCommandError
+
+
+class TestCommand(Command):
+
+    command_fmt = 'SayToPlayer {player_name!r} {message}'
+
+    def validate(self, reponse):
+        pass
 
 
 class CommandTests(TestCase):
 
     def setUp(self):
-        self.cmd = Command('do stuff')
+        self.parsed_args = Namespace(
+            player_name='Eggsy',
+            message='do stuff'
+        )
+        self.cmd = TestCommand(self.parsed_args)
 
-    def test_nice_default_success(self):
-        expected = fancy('{!r} succeeded.'.format('do stuff'), fg='green')
-        self.assertEquals(expected, self.cmd.success)
+    def test_str_to_command_text(self):
+        expected = "SayToPlayer 'Eggsy' do stuff"
+        actual = str(self.cmd)
+        self.assertEquals(expected, actual)
 
-    def test_nice_default_failure(self):
-        expected = fancy('{!r} failed!'.format('do stuff'), fg='red')
-        self.assertEquals(expected, self.cmd.failure)
+    def test_success(self):
+        expected = fancy(
+            '{!r} succeeded.'.format("SayToPlayer 'Eggsy' do stuff"),
+            fg='green'
+        )
+        self.assertEquals(expected, self.cmd.success())
 
-    def test_nice_default_response(self):
-        expected = fancy('{response}', fg='yellow')
-        self.assertEquals(expected, self.cmd.response)
+    def test_failure(self):
+        expected = fancy(
+            '{!r} failed!'.format("SayToPlayer 'Eggsy' do stuff"),
+            fg='red'
+        )
+        self.assertEquals(expected, self.cmd.failure())
 
-    def test_error_no_command(self):
-        with self.assertRaises(InvalidCommandError) as cm:
-            cmd = Command()
-        self.assertEquals('Expected a command string', str(cm.exception))
+    def test_server_response(self):
+        expected = fancy('I did stuff', fg='yellow')
+        self.assertEquals(expected, self.cmd.response('I did stuff'))
